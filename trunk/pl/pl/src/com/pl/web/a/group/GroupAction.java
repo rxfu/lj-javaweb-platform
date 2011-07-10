cc2b6662a432a07d5c19a9bc63426b39bdbab44a
space_key@163.com <space_key@163.com@870f3512-15be-11de-b69a-45dc1c5e4070>
2011-07-10 05:57:02 +0000

    移除警报

--- trunk/pl/pl/src/com/pl/web/a/group/GroupAction.java
+++ trunk/pl/pl/src/com/pl/web/a/group/GroupAction.java
@@ -2,8 +2,6 @@ package com.pl.web.a.group;
 
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.convention.annotation.Result;
 
 import com.pl.common.base.BaseAction;
